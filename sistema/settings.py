7212e6573dd82749509bb776a67244d2f6c45631
Redfieldz130 <109125123+Redfieldz130@users.noreply.github.com>
2025-03-04 16:01:09 -0400

    Add files via upload

--- sistema/settings.py
+++ sistema/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'sistema.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'inventario',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
